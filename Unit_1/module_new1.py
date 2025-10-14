732619122400e8ff861d12c04385ca7731244cc9
Your Name <you@example.com>
2025-10-14 09:53:57 +0530

    Modules_Functions

---
+++ Unit_1/module_new1.py
@@ -0,0 +1,17 @@
+#Python Program To create a New Module
+def add(a,b) :
+    c = a + b
+    return (c)
+
+def diff(a,b) :
+    c = a - b
+    return (c)
+
+def mul(a,b) :
+    c = a * b
+    return (c)
+
+def div(a,b) : 
+    c = a / b
+    return (c)
+
---
+++ Unit_1/module_new2.py
@@ -0,0 +1,8 @@
+#python Program to Import function
+import module_new1 as mn
+a = 100
+b = 20
+print(f"The sum of {a} and {b} is : {mn.add(a,b)}")
+print(f"The Difference of {a} and {b} is : {mn.diff(a,b)}")
+print(f"The Product of {a} and {b} is : {mn.mul(a,b)}")
+print(f"The Division of {a} and {b} is : {mn.div(a,b)}")
