5979800f3272bea2bd226c0300ff8fb296d0e828
Your Name <you@example.com>
2025-10-14 10:05:04 +0530

    Function with mul Return values

---
+++ Unit_1/diff_args.py
@@ -0,0 +1,9 @@
+def calculate(a,b):
+    sum_ = a + b
+    diff_ = a - b
+    prod_ = a * b
+    return (sum_, diff_, prod_)
+[x, y, z] = calculate(100, 20)
+print(f"The sum is : {x}")
+print(f"The Difference is : {y}")
+print(f"The Product is : {z}")
