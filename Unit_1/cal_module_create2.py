cf11fbd0dd9fe4055bbcc693ecbb9d54239d86e2
Your Name <you@example.com>
2025-10-21 10:38:08 +0530

    calculator_module

---
+++ Unit_1/cal_module_create2.py
@@ -0,0 +1,6 @@
+import cal_module_create1 as cal
+
+print("Addition:", cal.add(10, 5))
+print("Subtraction:", cal.sub(10, 5))
+print("Multiplication:", cal.mul(10, 5))
+print("Division:", cal.div(10, 5))
